61b547b19570a0bd550e75a63c60ba14f6bf9a20
safinathapa <thapasafina21@gmail.com>
2026-02-15 12:47:01 -0500

    Add Book Browsing and Sorting REST API endpoints

--- books/views.py
+++ books/views.py
@@ -1,11 +1,44 @@
-# Create your views here.
-from django.shortcuts import get_object_or_404, render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Book
 
+@api_view(['GET'])
 def book_list(request):
     books = Book.objects.all()
-    return render(request, "books/book_list.html", {"books": books})
+    
+    # Sorting
+    sort_by = request.GET.get('sort', 'title')
+    if sort_by in ['title', 'author', 'published_year']:
+        books = books.order_by(sort_by)
+    
+    # Convert to list of dictionaries
+    book_data = []
+    for book in books:
+        book_data.append({
+            'id': book.id,
+            'title': book.title,
+            'author': book.author,
+            'description': book.description,
+            'isbn': book.isbn,
+            'published_year': book.published_year,
+        })
+    
+    return Response(book_data)
 
+@api_view(['GET'])
 def book_detail(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    return render(request, "books/book_detail.html", {"book": book})
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response({'error': 'Book not found'}, status=404)
+    
+    book_data = {
+        'id': book.id,
+        'title': book.title,
+        'author': book.author,
+        'description': book.description,
+        'isbn': book.isbn,
+        'published_year': book.published_year,
+    }
+    
+    return Response(book_data)
