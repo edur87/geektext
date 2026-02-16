f85eb58ce0ff24961e36345189ba033e6b5d068a
Carlvensky St Fleur <tarots-hymn-0f@icloud.com>
2026-02-15 22:00:49 -0500

    update

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -90,6 +92,14 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+DATABASES_URL = os.getenv("DATABASE_URL")
+
+if DATABASES_URL:
+    DATABASES['default'] = dj_database_url.config(
+        default=DATABASES_URL,
+        conn_max_age=600,
+        ssl_require=True,
+    )
 
 
 # Password validation
